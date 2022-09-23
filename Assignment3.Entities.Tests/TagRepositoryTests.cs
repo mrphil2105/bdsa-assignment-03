7c8e932581ce84ac2f4d469c058cfdc5db52e49c
LocatedInSpace <48300341+LocatedInSpace@users.noreply.github.com>
2022-09-23 14:14:42 +0200

    started skeleton of tag tests

--- Assignment3.Entities.Tests/TagRepositoryTests.cs
+++ Assignment3.Entities.Tests/TagRepositoryTests.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Assignment3.Entities.Tests;
 
-public class TagRepositoryTests
+public class TagRepositoryTests : IDisposable
 {
+    private KanbanContext _context;
+    public TagRepositoryTests()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>();
+        optionsBuilder.UseInMemoryDatabase("KanbanDb");
 
+        _context = new KanbanContext(optionsBuilder.Options);
+    }
+    
+    [Fact]
+    public void Create_CreatesTagEntity_WhenGivenTagDTO()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 }
--- Assignment3.Entities/TagRepository.cs
+++ Assignment3.Entities/TagRepository.cs
@@ -1,5 +1,36 @@
+using Assignment3.Core;
+
 namespace Assignment3.Entities;
 
-public class TagRepository
+public class TagRepository : ITagRepository
 {
+    private KanbanContext _context;
+    public TagRepository(KanbanContext context)
+    {
+        _context = context;
+    }
+    public (Response Response, int TagId) Create(TagCreateDTO tag)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyCollection<TagDTO> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TagDTO Read(int tagId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Update(TagUpdateDTO tag)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Response Delete(int tagId, bool force = false)
+    {
+        throw new NotImplementedException();
+    }
 }
