894c01fb78c6ac1f6c19c6465bdc6c89856204c4
Philip Mørch <mrphil2105@gmail.com>
2022-09-29 19:21:00 +0200

    Make AssignedTo nullable to make it optional

---
+++ Assignment3.Entities/Migrations/20220929171558_AssignedToNullable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment3.Entities.Migrations
+{
+    public partial class AssignedToNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedToId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks",
+                column: "AssignedToId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AssignedToId",
+                table: "Tasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedToId",
+                table: "Tasks",
+                column: "AssignedToId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -6,7 +6,7 @@ public class Task
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public User AssignedTo { get; set; }
+    public User? AssignedTo { get; set; }
     public string Description { get; set; }
     public State State { get; set; }
     public ICollection<Tag> Tags { get; set; }
